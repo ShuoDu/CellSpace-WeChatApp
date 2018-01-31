d606f6b5ea94416773ee30992fb489093454d2c3
dushuo <shuo.du@edaibu.net>
2018-01-31 17:47:53 +0800

    第一次提交

---
+++ pages/goods/goods.js
@@ -0,0 +1,14 @@
+Page({
+  data: {
+    listData: [
+      { "code": "01", "text": "text1", "type": "type1" },
+      { "code": "02", "text": "text2", "type": "type2" },
+      { "code": "03", "text": "text3", "type": "type3" },
+      { "code": "04", "text": "text4", "type": "type4" }
+    ]
+  },
+  onLoad: function () {
+    console.log('onLoad')
+  }
+
+})
---
+++ pages/me/me.js
@@ -0,0 +1,32 @@
+Page({
+  data: {
+    listData: [
+      { "code": "01", "text": "订单" },
+      { "code": "02", "text": "格子" },
+      { "code": "03", "text": "收藏" },
+      { "code": "04", "text": "设置" },
+      { "code": "05", "text": "关于" }
+    ]
+  },
+  onLoad: function () {
+    console.log('onLoad')
+  }
+
+})
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
